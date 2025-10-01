941bf6885c2ce766649259a00befad927cfff9db
Nicolas <nicolas.somnier@epitech.eu>
2025-10-01 21:07:18 +0200

    {+} add particule animation, design modifidy}

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const cvBtn = document.querySelector("#cv");
 const cvOverlay = document.querySelector("#cv-overlay");
 const closeIcon = document.querySelector("#close-icon-id");
 const downloadIcon = document.querySelector("#download-icon-id");
+const container = document.querySelector("#container");
+const totalCircles = 100;
 
-
+for (let i = 0; i < totalCircles; i++) {
+    const circleContainer = document.createElement("div");
+    circleContainer.classList.add("circle-container"); // pour le parent
+  
+    const circle = document.createElement("div");
+    circle.classList.add("circle"); // pour l’enfant
+  
+    circleContainer.appendChild(circle); // on met le cercle dans le parent
+    container.appendChild(circleContainer); // on met le parent dans le container
+  }
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
