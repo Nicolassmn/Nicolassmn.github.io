d5b7d14d9da9a70afd7052307647eb549b5cbbff
Nicolas <nicolas.somnier@epitech.eu>
2025-10-01 18:48:47 +0200

    {+} responsivity header and home section}

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const menuIcon = document.querySelector('#menu-icon-id');
 const navbar = document.querySelector('#navBar');
+const cvBtn = document.querySelector("#cv");
+const cvOverlay = document.querySelector("#cv-overlay");
+const closeIcon = document.querySelector("#close-icon-id");
+const downloadIcon = document.querySelector("#download-icon-id");
+
 
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+});
+
+cvBtn.addEventListener('click', () => {
+    cvOverlay.classList.add('active');
+});
+
+cvOverlay.addEventListener('click', (e) =>{
+    if(e.target === cvOverlay) {
+        cvOverlay.classList.remove('active');
+    }
 });
